5d3bfb96f362a7d07562f18eeb92f0252a266138
Davos Oliveira <davosnd@gmail.com>
2018-09-07 14:26:53 -0300

    Bot Calculadora

---
+++ [BOT]Calculadora.py
@@ -0,0 +1,25 @@
+import discord, asyncio
+ 
+client = discord.Client()
+ 
+@client.event
+async def on_ready():
+    print('BOT ONLINE!')
+    print(client.user.name)
+    print(client.user.id)
+ 
+ 
+@client.event
+async def on_message(message):
+    if message.content.lower().startswith('?oi'):
+        await client.send_message(message.channel, 'Olá!')
+    elif message.content.lower().startswith('?birl'):
+        await client.send_message(message.channel, 'BIIIRLLLLL!')
+        
+        
+ 
+ 
+client.run('NDg3NjI4NDc5MDIyNDk3ODE2.DnQm6Q.jbALvHH79kUnhQyO8b-81wu9t2c')
+
+
+
