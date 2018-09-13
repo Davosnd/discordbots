53eac825882e1f4ff159e92c655426f0d896465b
Davos Oliveira <davosnd@gmail.com>
2018-09-12 23:41:37 -0300

    calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import discord
 import asyncio
  
@@ -17,37 +16,6 @@ async def on_message(message):
     elif message.content.lower().startswith('?test'):
         await client.send_message(message.channel, 'Ok!')
 
-client.run('NDg3NjI4NDc5MDIyNDk3ODE2.DnQm6Q.jbALvHH79kUnhQyO8b-81wu9t2c')
+client.run('')
 
 
-
-=======
-import discord
-import asyncio
- 
-client = discord.Client()
- 
-@client.event
-async def on_ready():
-    print('BOT ONLINE!')
-    print(client.user.name)
-    print(client.user.id)
- 
- 
-@client.event
-async def on_message(message):
-    if message.content.lower().startswith('?oi'):
-        await client.send_message(message.channel, 'Olá!')
-    elif message.content.lower().startswith('?birl'):
-        await client.send_message(message.channel, 'BIIIRLLLLL!')
-	elif message.content.lower().startswith('?game'):
-		await client.send_message(message.channel, ':)')
-        
-        
- 
- 
-client.run('NDg3NjI4NDc5MDIyNDk3ODE2.DnQm6Q.jbALvHH79kUnhQyO8b-81wu9t2c')
-
-
-
->>>>>>> 544f2e504581ffd3df3d7438524b2793591e1cc7
